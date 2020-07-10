d943fad6ddd806cbb7e238bb7b34c2bffca21176
goutarou <gouta.soccer.0220@outlook.jp>
2020-07-10 20:53:08 +0900

    二回目のコミットです。

--- app/src/main/java/com/example/gitstudy/MainActivity.kt
+++ app/src/main/java/com/example/gitstudy/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.gitstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("hasegawa", "hasegawaのログ")
     }
 }
